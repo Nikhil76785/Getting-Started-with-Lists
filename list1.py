5c84fc07abdba55db9dde9e7cbc3bf39bece159e
Nikhil76785 <valivetinikhil2@gmail.com>
2025-08-06 19:51:16 +0530

    LISTS

---
+++ list1.py
@@ -0,0 +1,18 @@
+empty_list = []
+print(empty_list)
+
+nums = [1, 2, 3, 4, 5]
+print(nums)
+
+triples = [1, 2, 3] * 3
+print(triples)
+
+a_list = [100, 200, 300, 400, 500]
+a_list = a_list[::-1]
+print(a_list)
+
+a = [10, 20, "Nikhil", 234.6, 25, 50]
+print(a[2:5])
+
+for i in a:
+    print(i)
